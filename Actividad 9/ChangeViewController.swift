bd01ee42f4299d0acc124876fc45d6bbb9ab830c
Alumno IDS <alumnoids@iMac-de-IT-Admin-6.local>
2018-03-08 15:10:36 -0600

    first commit

---
+++ Actividad 9/ChangeViewController.swift
@@ -0,0 +1,113 @@
+//
+//  ChangeViewController.swift
+//  Actividad 9
+//
+//  Created by Alumno IDS on 08/03/18.
+//  Copyright © 2018 Alumno IDS. All rights reserved.
+//
+
+import UIKit
+
+class ChangeViewController : UIViewController
+{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        colorGradiet()
+        setupObj()
+    }
+    func colorGradiet(){
+       let gradientBack = CAGradientLayer()
+        gradientBack.frame = self.view.bounds
+        gradientBack.colors = [UIColor.white.cgColor, UIColor.blue.cgColor]
+        self.view.layer.addSublayer(gradientBack)
+
+    }
+    
+    let inputContainerView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.masksToBounds = true
+        return view
+    }()
+    let SystemLabel : UILabel = {
+        let Label = UILabel()
+        Label.backgroundColor = .white
+        Label.translatesAutoresizingMaskIntoConstraints = false
+        return Label
+    }()
+    let YouLabel : UILabel = {
+        let Label = UILabel()
+        Label.backgroundColor = .white
+        Label.translatesAutoresizingMaskIntoConstraints = false
+        return Label
+    }()
+    let WinnerLabel: UILabel = {
+        let Label = UILabel()
+        Label.backgroundColor = .white
+        Label.translatesAutoresizingMaskIntoConstraints = false
+        Label.textAlignment = .center
+        return Label
+    }()
+
+    let BackButton : UIButton =
+    {
+        let BackButton = UIButton(type: .system)
+        BackButton.setTitle("Play Again", for: .normal)
+        BackButton.translatesAutoresizingMaskIntoConstraints = false
+        BackButton.addTarget(self, action: #selector(buttonRock(sender:)), for: .touchUpInside)
+        BackButton.backgroundColor = UIColor(red: 242/255, green: 10/255, blue: 0/255, alpha: 1)
+        BackButton.setTitleColor(.white, for: .normal)
+        return BackButton
+        
+    }()
+    
+    func buttonRock(sender: UIButton!) { //Action from button
+        //func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //  let destVC : ChangeViewController = segue.destination as! ChangeViewController
+        // destVC.dataFromFirst = "Hello there!"
+        //}
+        let logincontroller: LoginController = LoginController()
+        present(logincontroller, animated: true, completion: nil)
+    }
+    
+    func setupObj()
+    {
+        view.addSubview(inputContainerView)
+        inputContainerView.addSubview(SystemLabel)
+        inputContainerView.addSubview(YouLabel)
+        inputContainerView.addSubview(WinnerLabel)
+        view.addSubview(BackButton)
+        inputContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        inputContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        SystemLabel.topAnchor.constraint(equalTo: inputContainerView.topAnchor).isActive = true
+        SystemLabel.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        SystemLabel.leftAnchor.constraint(equalTo: inputContainerView.leftAnchor, constant: 12).isActive = true
+        SystemLabel.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        
+        
+        YouLabel.topAnchor.constraint(equalTo: SystemLabel.bottomAnchor).isActive = true
+        YouLabel.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        YouLabel.leftAnchor.constraint(equalTo: inputContainerView.leftAnchor, constant: 12).isActive = true
+        YouLabel.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+
+        
+        WinnerLabel.topAnchor.constraint(equalTo: YouLabel.bottomAnchor).isActive = true
+        WinnerLabel.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        WinnerLabel.leftAnchor.constraint(equalTo: inputContainerView.leftAnchor).isActive = true
+        WinnerLabel.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+
+        
+        
+        BackButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        BackButton.centerYAnchor.constraint(equalTo: view.centerYAnchor,constant: 100).isActive = true
+        BackButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        BackButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+    }
+    
+}
---
+++ Actividad 9/LoginController.swift
@@ -0,0 +1,186 @@
+//
+//  LoginController.swift
+//  Actividad 9
+//
+//  Created by Alumno IDS on 06/03/18.
+//  Copyright © 2018 Alumno IDS. All rights reserved.
+//
+
+import UIKit
+
+class LoginController:UIViewController
+{
+    var System:String?
+    var User:String?
+    var Result:String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 90/255 , green: 90/255, blue: 90/255, alpha: 1)
+        setupObj()
+    }
+    
+    let RockButton : UIButton =
+    {
+        let RockButton = UIButton(type: .system)
+        RockButton.setTitle("Rock", for: .normal)
+        RockButton.translatesAutoresizingMaskIntoConstraints = false
+        RockButton.addTarget(self, action: #selector(buttonRock(sender:)), for: .touchUpInside)
+        RockButton.backgroundColor = UIColor(red: 102/255, green: 51/255, blue: 0/255, alpha: 1)
+        RockButton.setTitleColor(.white, for: .normal)
+      
+        return RockButton
+        
+    }()
+    let PaperButton : UIButton =
+    {
+        let PaperButton = UIButton(type: .system)
+        PaperButton.setTitle("Scissors", for: .normal)
+        PaperButton.translatesAutoresizingMaskIntoConstraints = false
+        PaperButton.addTarget(self, action: #selector(buttonPaper(sender:)), for: .touchUpInside)
+        PaperButton.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 220/255, alpha: 1)
+        PaperButton.setTitleColor(.black, for: .normal)
+        return PaperButton
+        
+    }()
+
+    let ScissorsButton : UIButton =
+    {
+        let ScissorsButton = UIButton(type: .system)
+        ScissorsButton.setTitle("Paper", for: .normal)
+        ScissorsButton.translatesAutoresizingMaskIntoConstraints = false
+        ScissorsButton.addTarget(self, action: #selector(buttonScissors(sender:)), for: .touchUpInside)
+        ScissorsButton.backgroundColor = UIColor(red: 160/255, green: 160/255, blue: 160/255, alpha: 1)
+        ScissorsButton.setTitleColor(.white, for: .normal)
+        return ScissorsButton
+        
+    }()
+
+    func matchCalcule(option:Int)
+    {
+        let Random = Int(arc4random_uniform(3))
+       
+        //piedra, tijera, papel
+        
+        switch option{
+        case 0:
+            if Random == option
+            {
+                System = "Rock"
+                User = "Rock"
+                Result = "Draw"
+                print("Empate")
+            }else if Random == 1
+            {
+                System = "Scissors"
+                User = "Rock"
+                Result = "User Winner"
+                print("Winner")
+            }else if Random == 2
+            {
+                System = "Paper"
+                User = "Rock"
+                Result = "System Winner"
+                print("Losser")
+            }
+            
+            
+        case 1:
+            if Random == option
+            {
+                System = "Scissors"
+                User = "Scissors"
+                Result = "Draw"
+                print("Draw")
+            }else if Random == 0
+            {
+                System = "Rock"
+                User = "Scissors"
+                Result = "System Winner"
+                print("Losser")
+            }else if Random == 2
+            {
+                System = "Paper"
+                User = "Scissors"
+                Result = "User Winner"
+                print("Winner")
+            }
+            
+        case 2:
+            if Random == option
+            {
+                System = "Paper"
+                User = "Paper"
+                Result = "Draw"
+                print("Empate")
+            }else if Random == 1
+            {
+                System = "Scissors"
+                User = "Paper"
+                Result = "System Winner"
+                print("Losser")
+            }else if Random == 0
+            {
+                System = "Rock"
+                User = "Paper"
+                Result = "User Winner"
+                print("Winner")
+            }
+            
+        default:
+            
+            print("Option invalite")
+            
+        }
+
+        
+    }
+    
+    func buttonRock(sender: UIButton!) { //Action from button
+             let changeviewcontroller: ChangeViewController = ChangeViewController()
+        matchCalcule(option: 0)
+        changeviewcontroller.SystemLabel.text = "System: "+System!
+        changeviewcontroller.YouLabel.text = "You: " + User!
+        changeviewcontroller.WinnerLabel.text = "Result: " + Result!
+         present(changeviewcontroller, animated: true, completion: nil)
+     
+    }
+    func buttonPaper(sender: UIButton!) { //Action from button
+        let changeviewcontroller: ChangeViewController = ChangeViewController()
+        matchCalcule(option: 1)
+        changeviewcontroller.SystemLabel.text = "System: "+System!
+        changeviewcontroller.YouLabel.text = "You: " + User!
+        changeviewcontroller.WinnerLabel.text = "Result: " + Result!
+        present(changeviewcontroller, animated: true, completion: nil)
+    }
+    func buttonScissors(sender: UIButton!) { //Action from button
+        let changeviewcontroller: ChangeViewController = ChangeViewController()
+        matchCalcule(option: 2)
+        changeviewcontroller.SystemLabel.text = "System: "+System!
+        changeviewcontroller.YouLabel.text = "You: " + User!
+        changeviewcontroller.WinnerLabel.text = "Result: " + Result!
+        present(changeviewcontroller, animated: true, completion: nil)
+    }
+    func setupObj()
+    {
+        view.addSubview(RockButton)
+        view.addSubview(PaperButton)
+        view.addSubview(ScissorsButton)
+        RockButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        RockButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        RockButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        RockButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 2/3).isActive = true
+        //Set width with func multiplier
+        
+        PaperButton.topAnchor.constraint(equalTo: RockButton.bottomAnchor,constant: 15).isActive = true
+        PaperButton.widthAnchor.constraint(equalTo: RockButton.widthAnchor).isActive = true
+        PaperButton.heightAnchor.constraint(equalTo: RockButton.heightAnchor).isActive = true
+        PaperButton.leftAnchor.constraint(equalTo: RockButton.leftAnchor).isActive = true
+        
+        ScissorsButton.topAnchor.constraint(equalTo: PaperButton.bottomAnchor,constant: 15).isActive = true
+        ScissorsButton.widthAnchor.constraint(equalTo: PaperButton.widthAnchor).isActive = true
+        ScissorsButton.heightAnchor.constraint(equalTo: PaperButton.heightAnchor).isActive = true
+        ScissorsButton.leftAnchor.constraint(equalTo: PaperButton.leftAnchor).isActive = true
+    }
+    
+
+}
